c385eca22c96c6b99f92a3f31de882a6f18f5755
AbuAbdulmalik <mr.betterdoctor@gmail.com>
2024-04-04 07:35:42 +0300

    список покупок

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+class Item{
+  purchase: string;
+  done: boolean;
+  price: number;
+
+  constructor(purchase: string, price: number) {
+
+      this.purchase = purchase;
+      this.price = price;
+      this.done = false;
+  }
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'metanit';
+  text: string = "";
+  price: number = 0;
+
+  items: Item[] =
+  [
+      { purchase: "Хлеб", done: false, price: 15.9 },
+      { purchase: "Масло", done: false, price: 60 },
+      { purchase: "Картофель", done: true, price: 22.6 },
+      { purchase: "Сыр", done: false, price:310 }
+  ];
+  addItem(text: string, price: number): void {
+
+      if(text==null || text.trim()=="" || price==null)
+          return;
+      this.items.push(new Item(text, price));
+  }
 }
